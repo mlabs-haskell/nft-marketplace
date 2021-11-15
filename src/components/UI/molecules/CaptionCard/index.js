e4c83f2a20dee03909156629e3730aa93a04a402
Firdausi1 <firdausib18@gmail.com>
2021-11-15 21:01:26 +0100

    Home page UI

--- src/components/UI/molecules/CaptionCard/index.js
+++ src/components/UI/molecules/CaptionCard/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import Box from '../../atoms/Box'
-import styles from './index.module.scss'
 
 function CaptionCard({title, name, className}) {
     return (
-        <Box boxClass= {styles.card}>
+        <Box boxClass= {className}>
             <h3>{title}</h3>
             <p> <span>{name}</span></p>
         </Box>
