20c9071a617fccf51ab1f5c71f3bdecc89fa7c6e
vagmen <vagizahmetov536@gmail.com>
2021-12-08 15:36:06 +0300

    Fixed the scrolling bug of itempage

--- src/pages/ItemPage/index.tsx
+++ src/pages/ItemPage/index.tsx
@@ -1,20 +1,32 @@
 import ItemDetails from 'components/UI/molecules/ItemDetails';
 import ItemPhotoCard from 'components/UI/molecules/ItemPhotoCard';
+import { useEffect, useState } from 'react';
 import styles from './index.module.scss';
 
-const ItemPage = () => (
-  <div className={styles.container}>
-    <ItemPhotoCard likeCount="167" />
-    <ItemDetails
-      title="SPIRIT SEED - SYNTHETIC"
-      saleValue="250ETH"
-      topBidValue="1.123WETH"
-      description="Syntertic Seeds cannot be bought on the primary marktet and can only be earned or gifted. Synthetic Seeds do not belong to any Tessellation Class, cannot be incubated and are not eligible to be ..."
-      creatorValue="10% royalties"
-      creatorName="Defacer#od"
-      type="SELL"
-    />
-  </div>
-);
+const ItemPage = () => {
+  const [fristTime, setFristTime] = useState(true);
+
+  useEffect(() => {
+    if (fristTime) {
+      window.scrollTo(0, 20);
+      setFristTime(false);
+    }
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <ItemPhotoCard likeCount="167" />
+      <ItemDetails
+        title="SPIRIT SEED - SYNTHETIC"
+        saleValue="250ETH"
+        topBidValue="1.123WETH"
+        description="Syntertic Seeds cannot be bought on the primary marktet and can only be earned or gifted. Synthetic Seeds do not belong to any Tessellation Class, cannot be incubated and are not eligible to be ..."
+        creatorValue="10% royalties"
+        creatorName="Defacer#od"
+        type="SELL"
+      />
+    </div>
+  );
+};
 
 export default ItemPage;
