6dc2e90a65122ca58ba98c29516c5b5860e2b783
Neil Rutledge <neilrutledgephoto@gmail.com>
2021-12-09 03:16:36 -0500

    Improve explore section grid alignment

--- src/components/UI/organisms/Home/Explore/index.tsx
+++ src/components/UI/organisms/Home/Explore/index.tsx
@@ -1,6 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useMemo, useState } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
-import { InformationNft } from 'seabug-sdk/src/common';
 import { NftContextType } from '../../../../../context/NftContext';
 import Button from '../../../atoms/Button';
 import AuctionCard from '../../../molecules/AuctionCard';
@@ -12,26 +11,14 @@ interface Props {
 }
 
 const Explore = ({ images, NFTs }: Props) => {
-  function limitNfts(limit: number) {
-    const listNfts: InformationNft[] = [];
-    NFTs.forEach((nft: InformationNft, i: number) => {
-      if (i < limit) {
-        listNfts.push(nft);
-      }
-    });
-    return listNfts;
-  }
-
-  const [limitedNfts, setLimitedNfts] = useState(limitNfts(25));
   const [currentPage, setCurrentPage] = useState(1);
 
-  useEffect(() => {
-    setLimitedNfts(limitNfts(25));
-  }, [NFTs]);
+  const cardsPerPage = 25;
 
-  useEffect(() => {
-    setLimitedNfts(limitNfts(25 * currentPage));
-  }, [currentPage]);
+  const limitedNfts = useMemo(
+    () => NFTs.slice(0, currentPage * cardsPerPage),
+    [NFTs, currentPage]
+  );
 
   return (
     <div className={styles.contatiner}>
@@ -42,39 +29,36 @@ const Explore = ({ images, NFTs }: Props) => {
           <Button label="My Collection" color="primary" />
         </div>
       </div>
-      <div className={styles['card-container']}>
-        <InfiniteScroll
-          dataLength={limitedNfts.length} // This is important field to render the next data
-          next={() => {
-            if (currentPage !== 1) setCurrentPage(currentPage + 1);
-          }}
-          hasMore
-          loader={Array(25).map((value, i) => (
-            <AuctionCard key={i.toString()} amount="0.005 ETH " isExplore />
+      <InfiniteScroll
+        dataLength={limitedNfts.length} // This is important field to render the next data
+        next={() => {
+          if (currentPage !== 1) setCurrentPage(currentPage + 1);
+        }}
+        hasMore
+        loader={Array(25).map((value, i) => (
+          <AuctionCard key={i.toString()} amount="" isExplore />
+        ))}
+        scrollableTarget={styles['card-container']}
+        endMessage={
+          <p style={{ textAlign: 'center' }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+        scrollThreshold="80%"
+        // below props only if you need pull down functionality
+      >
+        <div className={styles['card-container']}>
+          {limitedNfts.map((nft) => (
+            <AuctionCard
+              key={images.get(nft.id.contentHash)?.sha256hash}
+              amount="0.005 ETH "
+              title={images.get(nft.id.contentHash)?.title}
+              image={images.get(nft.id.contentHash)?.path}
+              isExplore
+            />
           ))}
-          scrollableTarget={styles['card-container']}
-          endMessage={
-            <p style={{ textAlign: 'center' }}>
-              <b>Yay! You have seen it all</b>
-            </p>
-          }
-          scrollThreshold="80%"
-          // below props only if you need pull down functionality
-        >
-          {limitedNfts.map(
-            (nft) =>
-              images.get(nft.id.contentHash) && (
-                <AuctionCard
-                  key={images.get(nft.id.contentHash)?.sha256hash}
-                  amount="0.005 ETH "
-                  title={images.get(nft.id.contentHash)?.title}
-                  image={images.get(nft.id.contentHash)?.path}
-                  isExplore
-                />
-              )
-          )}
-        </InfiniteScroll>
-      </div>
+        </div>
+      </InfiniteScroll>
       <div className={styles.btn}>
         {currentPage === 1 ? (
           <Button
