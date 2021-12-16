f803b04a79b454acf06827190a8b0b8574704c42
Neil Rutledge <neilrutledgephoto@gmail.com>
2021-12-15 23:11:10 -0500

    Add fields and mock data to WalletContext

--- src/context/WalletContext.tsx
+++ src/context/WalletContext.tsx
@@ -4,32 +4,64 @@ export type WalletAddress = {
   pubKeyHash: string;
 };
 
-export type Wallet =
-  | { name: 'Test'; address: WalletAddress }
-  | { name: 'None' };
+export type WalletName = 'NONE' | 'TEST';
+
+export type TransactionCborHex = string;
+
+export type TransactionHash = string;
 
 export type WalletContextType = {
-  connected: Wallet;
-  connectTestWallet: (password: string) => void;
+  wallets: WalletName[];
+  connected: WalletName;
+  getPubKeyHashes: () => Promise<string[]>;
+  signAndSubmitTx: (tx: TransactionCborHex) => Promise<TransactionHash>;
+  connect: (wallet: WalletName) => void;
 };
 
 export const WalletContext = createContext<WalletContextType>({
-  connected: { name: 'None' },
-  connectTestWallet: (password: string) => {},
+  wallets: [],
+  connected: 'NONE',
+  getPubKeyHashes: () => Promise.resolve([]),
+  signAndSubmitTx: () => Promise.resolve(''),
+  connect: () => {},
 });
 
 // TODO: Implement actual wallet connection logic
 export const WalletContextProvider: FC = ({ children }) => {
-  const [connected, setConnected] = useState<Wallet>({ name: 'None' });
+  const [wallets, setWallets] = useState<WalletName[]>([]);
+  const [connected, setConnected] = useState<WalletName>('NONE');
 
-  const connectTestWallet = (password: string) =>
-    setConnected({ name: 'Test', address: { pubKeyHash: 'ff00000000' } });
+  const connect = (wallet: WalletName): void => {
+    if (!wallets.includes(wallet)) {
+      console.error(
+        `Attempted to connect to wallet (${wallet}) that is not available`
+      );
+      return;
+    }
+
+    setConnected(wallet);
+  };
+
+  const getPubKeyHashes = () => {
+    switch (connected) {
+      case 'TEST':
+        return Promise.resolve(['ff00000001']);
+      default:
+        return Promise.resolve([]);
+    }
+  };
+
+  const signAndSubmitTx = (tx: TransactionCborHex) =>
+    Promise.resolve('abcd1234');
 
   return (
     <WalletContext.Provider
       value={{
+        wallets,
         connected,
-        connectTestWallet,
+        getPubKeyHashes,
+        connect,
+        signAndSubmitTx,
       }}
     >
       {children}
