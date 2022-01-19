c36026944c73ae1fc4108021abd493e2b56b4e9e
Firdausi1 <firdausib18@gmail.com>
2022-01-19 14:20:14 +0100

    Filter by sales and collection added

--- src/components/UI/organisms/Home/Explore/index.tsx
+++ src/components/UI/organisms/Home/Explore/index.tsx
@@ -1,5 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
+import ExploreHeader from 'components/UI/molecules/ExploreHeader';
+import { useWalletContext } from 'context/WalletContext';
 import InfiniteScroll from 'react-infinite-scroll-component';
+import { InformationNft } from 'seabug-sdk/src/common';
 import { NftContextType } from '../../../../../context/NftContext';
 import Button from '../../../atoms/Button';
 import AuctionCard from '../../../molecules/AuctionCard';
@@ -19,14 +22,34 @@ const Explore = ({ images, getImageByNftId, nfts }: Props) => {
       ? parseInt(ticker?.replace('LOAD', ''), 10)
       : 1;
   };
-  const [currentPage, setCurrentPage] = useState(watchTicker());
   const cardsPerPage = 25;
-  const limitedNfts = nfts.slice(0, currentPage * cardsPerPage);
+  const [currentPage, setCurrentPage] = useState(watchTicker());
+  const [limitedNfts, setLimitedNfts] = useState<InformationNft[]>([]);
+  // const limitedNfts = nfts.slice(0, currentPage * cardsPerPage);
 
   const [scrollPosition, setScrollPosition] = useState(0);
   const handleScroll = () => {
     const position = window.pageYOffset;
     setScrollPosition(position);
+  };
+  const [walletsPubKeyHashes, setWalletsPubKeyHashes] = useState<string[]>([]);
+  const { getPubKeyHashes, connected } = useWalletContext();
+
+  const getLimitedNfts = () => {
+    setLimitedNfts(nfts.slice(0, currentPage * cardsPerPage));
+  };
+  const getCollections = () => {
+    const artistCollections = nfts.filter(
+      (item) => item.owner.pubKeyHash === walletsPubKeyHashes[0]
+    );
+    setLimitedNfts(artistCollections);
+  };
+  const getSales = () => {
+    const artistSales = nfts.filter(
+      (item) =>
+        item.owner.pubKeyHash === walletsPubKeyHashes[0] && item.auctionState
+    );
+    setLimitedNfts(artistSales);
   };
   useEffect(() => {
     const homeScrollPosition = parseInt(
@@ -47,49 +70,62 @@ const Explore = ({ images, getImageByNftId, nfts }: Props) => {
   useEffect(() => {
     localStorage.setItem('ticker', JSON.stringify(currentPage));
   }, [currentPage]);
-
+  useEffect(() => {
+    // TODO
+    const refreshPubKey = async () => {
+      const pubKeyHashes = await getPubKeyHashes();
+      setWalletsPubKeyHashes(pubKeyHashes);
+    };
+    refreshPubKey();
+  }, [connected]);
+  useEffect(() => {
+    getLimitedNfts();
+  }, [nfts]);
   return (
-    <div className={styles.contatiner}>
-      <InfiniteScroll
-        dataLength={limitedNfts.length} // This is important field to render the next data
-        next={() => {
-          if (currentPage !== 1) setCurrentPage(currentPage + 1);
-        }}
-        hasMore
-        loader={Array(25).map((value, i) => (
-          <AuctionCard key={i.toString()} />
-        ))}
-        scrollableTarget={styles['card-container']}
-        endMessage={
-          <p style={{ textAlign: 'center' }}>
-            <b>Yay! You have seen it all</b>
-          </p>
-        }
-        scrollThreshold="80%"
-        // below props only if you need pull down functionality
-      >
-        <div className={styles['card-container']}>
-          {limitedNfts.map((nft) => {
-            const image = getImageByNftId(nft.id);
-            return (
-              <AuctionCard key={nft.id.contentHash} nft={nft} image={image} />
-            );
-          })}
+    <>
+      <ExploreHeader collections={getCollections} sales={getSales} />
+      <div className={styles.contatiner}>
+        <InfiniteScroll
+          dataLength={limitedNfts.length} // This is important field to render the next data
+          next={() => {
+            if (currentPage !== 1) setCurrentPage(currentPage + 1);
+          }}
+          hasMore
+          loader={Array(25).map((value, i) => (
+            <AuctionCard key={i.toString()} />
+          ))}
+          scrollableTarget={styles['card-container']}
+          endMessage={
+            <p style={{ textAlign: 'center' }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+          scrollThreshold="80%"
+          // below props only if you need pull down functionality
+        >
+          <div className={styles['card-container']}>
+            {limitedNfts.map((nft) => {
+              const image = getImageByNftId(nft.id);
+              return (
+                <AuctionCard key={nft.id.contentHash} nft={nft} image={image} />
+              );
+            })}
+          </div>
+        </InfiniteScroll>
+        <div className={styles.btn}>
+          {currentPage === 1 ? (
+            <Button
+              label="Load More"
+              color="primary"
+              size="large"
+              onClick={() => setCurrentPage(currentPage + 1)}
+            />
+          ) : (
+            <></>
+          )}
         </div>
-      </InfiniteScroll>
-      <div className={styles.btn}>
-        {currentPage === 1 ? (
-          <Button
-            label="Load More"
-            color="primary"
-            size="large"
-            onClick={() => setCurrentPage(currentPage + 1)}
-          />
-        ) : (
-          <></>
-        )}
       </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,6 @@ import { NftContext } from 'context/NftContext';
 import Auction from 'components/UI/organisms/Home/Auction';
 import Explore from 'components/UI/organisms/Home/Explore';
 import Header from 'components/UI/organisms/Home/Header';
-import ExploreHeader from 'components/UI/molecules/ExploreHeader';
 
 const Home = () => {
   const { artists, images, nfts, common } = useContext(NftContext);
@@ -16,7 +15,6 @@ const Home = () => {
     <div>
       <Header artists={artists.listRandomized} />
       {nfts.getLiveAuctionList().length > 0 && <Auction />}
-      <ExploreHeader />
       <Explore
         images={images.list}
         getImageByNftId={images.getByNftId}
