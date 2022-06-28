6d5fd941a583f41ccc992e08ee32388ba42ec1a1
Aleksandr Penskoi <aleksandr@mlabs.city>
2022-06-28 03:45:18 +0300

    Set logLevel to Trace.

--- src/ctl/index.ts
+++ src/ctl/index.ts
@@ -15,6 +15,7 @@ const ctlConfig = {
   datumCacheSecureConn: appConfig.ctl.datumCache.secureConn,
   networkId: appConfig.ctl.networkId,
   projectId: appConfig.ctl.projectId,
+  logLevel: 'Trace',
 };
 
 let ctlSeabug: typeof import('seabug-contracts');
