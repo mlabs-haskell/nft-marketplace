db770989f41ee0f93dde109907e02016272bf47e
Andrea Ciceri <andrea.ciceri@autistici.org>
2022-11-11 15:33:29 +0100

    Module options for derivation building

--- flake.nix
+++ flake.nix
@@ -44,6 +44,35 @@
             }
           ];
         };
+
+      artifactsBuilder = system: envVars:
+        let
+          nft-marketplace-frontend = d2nFlakeOutputs.packages.${system}.default.overrideAttrs (old: {
+            preBuild =
+              let
+                seabug-contracts-output-js = seabug-contracts.packages.${system}.seabug-contracts.overrideAttrs (old: {
+                  buildCommand = old.buildCommand + ''
+                    cp output.js $out
+                  '';
+                });
+              in
+              (old.preBuild or "") + ''
+                mkdir seabug-contracts-module
+                cp -r $out/lib/node_modules/nft-marketplace/node_modules/seabug-contracts/* seabug-contracts-module
+                cp ${seabug-contracts-output-js}/output.js seabug-contracts-module
+                rm -rf $out/lib/node_modules/nft-marketplace/node_modules/seabug-contracts
+                cp -r seabug-contracts-module $out/lib/node_modules/nft-marketplace/node_modules/seabug-contracts
+              '';
+          } // envVars);
+        in
+        pkgsFor.${system}.stdenv.mkDerivation {
+          name = "nft-marketplace";
+          dontUnpack = true;
+          dontBuild = true;
+          installPhase = ''
+            cp -r ${nft-marketplace-frontend}/lib/node_modules/nft-marketplace/build $out
+          '';
+        };
     in
     {
       # The following add a check that run `npm run test` (i.e. `craco test`)
@@ -59,52 +88,9 @@
       #   });
       # });
 
-      packages = perSystem (system: {
-        nft-marketplace-frontend-artifacts =
-          let
-            nft-marketplace-frontend = d2nFlakeOutputs.packages.${system}.default.overrideAttrs (old: {
-              SKIP_PREFLIGHT_CHECK = true; # TODO do we really need this?
-              REACT_APP_API_BASE_URL = "https://api.seabug.staging.mlabs.city";
-              REACT_APP_CTL_LOG_LEVEL = "Trace";
-              REACT_APP_CTL_SERVER_HOST = "ctl-server.preview-seabug.ctl-runtime.staging.mlabs.city";
-              REACT_APP_CTL_SERVER_PORT = "443";
-              REACT_APP_CTL_SERVER_SECURE_CONN = "true";
-              REACT_APP_CTL_OGMIOS_HOST = "ogmios.preview-seabug.ctl-runtime.staging.mlabs.city";
-              REACT_APP_CTL_OGMIOS_PORT = "443";
-              REACT_APP_CTL_OGMIOS_SECURE_CONN = "true";
-              REACT_APP_CTL_DATUM_CACHE_HOST = "ogmios-datum-cache.preview-seabug.ctl-runtime.staging.mlabs.city";
-              REACT_APP_CTL_DATUM_CACHE_PORT = "443";
-              REACT_APP_CTL_DATUM_CACHE_SECURE_CONN = "true";
-              REACT_APP_CTL_NETWORK_ID = "0";
-              REACT_APP_CTL_PROJECT_ID = "previewoXa2yw1U0z39X4VmTs6hstw4c6cPx1LN";
-              REACT_APP_IPFS_BASE_URL = "https://cloudflare-ipfs.com/ipfs/";
 
-              preBuild =
-                let
-                  seabug-contracts-output-js = seabug-contracts.packages.${system}.seabug-contracts.overrideAttrs (old: {
-                    buildCommand = old.buildCommand + ''
-                      cp output.js $out
-                    '';
-                  });
-                in
-                (old.preBuild or "") + ''
-                  mkdir seabug-contracts-module
-                  cp -r $out/lib/node_modules/nft-marketplace/node_modules/seabug-contracts/* seabug-contracts-module
-                  cp ${seabug-contracts-output-js}/output.js seabug-contracts-module
-                  rm -rf $out/lib/node_modules/nft-marketplace/node_modules/seabug-contracts
-                  cp -r seabug-contracts-module $out/lib/node_modules/nft-marketplace/node_modules/seabug-contracts
-                '';
-            });
-          in
-          pkgsFor.${system}.stdenv.mkDerivation {
-            name = "nft-marketplace";
-            dontUnpack = true;
-            dontBuild = true;
-            installPhase = ''
-              cp -r ${nft-marketplace-frontend}/lib/node_modules/nft-marketplace/build $out
-            '';
-          };
-        default = self.packages.${system}.nft-marketplace-frontend-artifacts;
+      packages = perSystem (system: {
+        default = d2nFlakeOutputs.packages.${system}.default;
         inherit (d2nFlakeOutputs.packages.${system}) resolveImpure;
       });
 
@@ -115,31 +101,29 @@
       });
 
       nixosModules = {
-        nft-marketplace-frontend = {
+        nft-marketplace-frontend = { pkgs, ... }: {
           imports = [ ./nix/nft-marketplace-frontend.nix ];
-          nixpkgs.overlays = [ self.overlays.nft-marketplace-frontend ];
+          services.nft-marketplace-frontend.artifactsBuilder = artifactsBuilder pkgs.system;
         };
         default = self.nixosModules.nft-marketplace-frontend;
-      };
-
-      overlays = {
-        nft-marketplace-frontend = pkgsSelf: pkgsSuper: {
-          nft-marketplace-frontend-artifacts = self.packages.${pkgsSelf.system}.nft-marketplace-frontend-artifacts;
-        };
-        default = self.overlays.nft-marketplace-frontend;
       };
 
       nixosConfigurations.test = lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           self.nixosModules.default
-          ./test-module.nix
+          ({ modulesPath, ... }: {
+            imports = [
+              "${modulesPath}/virtualisation/qemu-vm.nix"
+            ];
+            services.nft-marketplace-frontend.enable = true;
+          })
         ];
       };
 
       hydraJobs = {
         inherit (self.packages.x86_64-linux) nft-marketplace-frontend-artifacts;
-        nixos-module = self.nixosConfigurations.test.system.build.toplevel;
+        test-nixos-module = self.nixosConfigurations.test.config.system.build.toplevel;
       };
     };
 }
--- nix/test.nix
+++
@@ -1,9 +0,0 @@
-{ config, lib, pkgs, modulesPath, ... }:
-
-{
-  imports = [
-    ./nft-marketplace-frontend.nix
-  ];
-
-  services.nft-marketplace-frontend.enable = true;
-}
